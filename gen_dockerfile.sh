80ad5dffaeb15d1bd5542bd0a2bd72b298426813
genghuiluo <luogenghui@gmail.com>
2017-07-21 17:43:34 +0800

    test.sql

--- gen_dockerfile.sh
+++ gen_dockerfile.sh
@@ -3,8 +3,11 @@
 # pre-push hookk in .git/hooks
 # generate dockerfile between two tags
 
-remote_tag=$(git ls-remote --tags origin|tail -1)
+remote_tag=$1
 local_tag=$(git tag|tail -1)
+
+echo  $remote_tag
+echo  $local_tag
 
 git diff $remote_tag $local_tag --name-only
 
